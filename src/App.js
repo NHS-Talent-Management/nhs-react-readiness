60613ba86360186fafd3f39e7ad497ff457a89f8
Arturs Karlovics# <arturs.karlovics@placecube.com>
2020-03-30 12:03:51 +0100

    react app changes to work in tomcat

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
     return (
         <div className="App">
-            <BrowserRouter>
+            <BrowserRouter basename={'/readiness'} >
                 <Header/>
                 <Content/>
                 <Footer/>
